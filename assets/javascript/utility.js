8b84d9cee733fcece5d5975209f4ab2d4f267f97
lostphilosopher <wandersen02@gmail.com>
2015-10-23 23:19:13 -0500

    Countdown clock and clickable photos

--- assets/javascript/utility.js
+++ assets/javascript/utility.js
@@ -4,7 +4,22 @@
 
 // Show / Hide Demo
 $( document ).ready(function() {
-  $( "#utility-js-demo-button" ).click(function() {
-    $( "#utility-js-demo-target" ).toggle();
-  });
+  var end = new Date('04/30/2016 2:30 PM');
+  var _second = 1000;
+  var _minute = _second * 60;
+  var _hour = _minute * 60;
+  var _day = _hour * 24;
+  var timer;
+  function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+      clearInterval(timer);
+      document.getElementById('countdown').innerHTML = '';
+      return;
+    }
+    var days = Math.floor(distance / _day);
+    document.getElementById('countdown').innerHTML = days + ' Days Away';
+  }
+  timer = setInterval(showRemaining, 1000);
 });
